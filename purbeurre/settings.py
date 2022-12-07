3fe78fd8719fe5aa91e6a0ca27fc7eb4707cb87f
Claire <biedermann.claire@gmail.com>
2022-12-07 06:03:26 -0500

    changement clef heroku

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -144,8 +144,8 @@ django_heroku.settings(locals())
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER =
-EMAIL_HOST_PASSWORD =
+EMAIL_HOST_USER = 'biedermannclaire153@gmail.com'
+EMAIL_HOST_PASSWORD = 'ehazvotpsnaptfgs' #past the key or password app here
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'biedermannclaire153@gmail.com'
